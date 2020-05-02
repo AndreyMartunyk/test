d16a109390771768c47d5edaabcfccf4f93ccf7c
Andrey Martunyk <aniks.zoom@gmail.com>
2020-05-02 04:47:39 +0300

    Initial commit

---
+++ test/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            using (Tester tester = new Tester())
+            {
+
+                long[] res = tester.MakeSpeedTest(15, DoEasyWork);
+                long sum = 0;
+                for (int i = 0; i < res.Length; i++)
+                {
+                    sum += res[i];
+                    Console.WriteLine("test number {0}: {1}", i + 1, res[i]);
+                }
+                Console.WriteLine("average work: " + sum / res.Length);
+                Console.WriteLine();
+            }
+
+            using (Tester tester = new Tester())
+            {
+                long[] res = tester.MakeSpeedTestAsync(15, DoEasyWork).Result;
+                long sum = 0;
+                for (int i = 0; i < res.Length; i++)
+                {
+                    sum += res[i];
+                    Console.WriteLine("async test number {0}: {1}", i + 1, res[i]);
+                }
+                Console.WriteLine("average work async: " + sum / res.Length);
+                Console.WriteLine();
+            }
+
+            using (Tester tester = new Tester())
+            {
+                long[] res = tester.MakeSpeedTest(5, DoHardWork);
+                long sum = 0;
+                for (int i = 0; i < res.Length; i++)
+                {
+                    sum += res[i];
+                    Console.WriteLine("test number {0}: {1}", i + 1, res[i]);
+                }
+                Console.WriteLine("average hard work: " + sum / res.Length);
+                Console.WriteLine();
+            }
+
+            using (Tester tester = new Tester())
+            {
+                long[] res = tester.MakeSpeedTestAsync(5, DoHardWork).Result;
+                long sum = 0;
+                for (int i = 0; i < res.Length; i++)
+                {
+                    sum += res[i];
+                    Console.WriteLine("async test number {0}: {1}", i + 1, res[i]);
+                }
+                Console.WriteLine("average hard work async: " + sum / res.Length);
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+        static void DoEasyWork()
+        {
+            for (int i = 0; i < 100000000; i++)
+            {
+            }
+        }
+
+        static void DoHardWork()
+        {
+            long res = 0;
+            for (int i = 0; i < 100000000; i++)
+            {
+                res *= i;
+            }
+        }
+
+        #region just tests
+        static async Task<string> ReadLineAsync()
+        {
+            var res = await Task.Run(() => ReadLine());
+            return res;
+        }
+
+        static string ReadLine()
+        {
+            return Console.ReadLine();
+        }
+
+        //static async Task<bool> SaveFileAsync(string path)
+        //{
+        //    var result = await Task.Run(() => SaveFile(path));
+        //    return result;
+        //}
+
+        //static bool SaveFile(string path = "d:\\test.txt")
+        //{
+        //    lock (locker) 
+        //    {
+        //        var rnd = new Random();
+        //        var text = "";
+        //        for (int i = 0; i < 40000; i++)
+        //        {
+        //            text += rnd.Next();
+        //        }
+        //    } 
+
+        //    using(var sw = new StreamWriter(path, false, Encoding.UTF8))
+        //    {
+        //        sw.WriteLine();
+        //    }
+
+        //    return true;
+        //}
+
+        static async Task DoWorkAsync()
+        {
+            await Task.Run(() => DoEasyWork());
+            Console.WriteLine("DoWorkAsynk");
+        }
+
+        static async Task DoWorkAsync(int max)
+        {
+            await Task.Run(() => DoWork(max));
+            Console.WriteLine("End DoWorkAsynk");
+        }
+
+        static void DoWork(object max)
+        {
+            for (int i = 0; i < (int)max; i++)
+            {
+                Console.WriteLine("DoWork");
+            }
+        }
+        #endregion
+
+        
+
+        
+    }
+}
---
+++ test/Tester.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace test
+{
+    class Tester: IDisposable
+    {
+        Stopwatch sw;
+        public delegate void Callback();
+        
+        public Tester()
+        {
+            sw = new Stopwatch();
+        }
+
+        public long[] MakeSpeedTest(int countTests = 1, params Callback[] callback)
+        {
+            long[] res = new long[callback.Length * countTests];
+
+            for (int y = 0; y < countTests; y++)
+            {
+                if (countTests < 1)
+                {
+                    return null;
+                }
+                for (int i = 0; i < callback.Length; i++)
+                {
+                    sw.Start();
+                    callback[i]();
+                    sw.Stop();
+                    res[i + y] = sw.ElapsedTicks;
+                    sw.Reset();
+                }
+            }
+            
+            return  res;
+        }
+
+        public async Task<long[]> MakeSpeedTestAsync(int countTests = 1, params Callback[] callback)
+        {
+            var result = await Task.Run(() => MakeSpeedTest(countTests, callback));
+            return result;
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Disposed Tester");
+        }
+    }
+}
